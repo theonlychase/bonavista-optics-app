f1aca9d6380c8acd5f49c38ef31bb65910d22b9c
Chase Isley <isleychase@gmail.com>
2016-04-28 12:35:41 -0700

    changed phone number

--- www/scripts/home/home-data.service.js
+++ www/scripts/home/home-data.service.js
@@ -10,7 +10,7 @@
 	/* @ngInject */
 	function homeDataService() {
 		return {
-			phoneNumber: '+18009239730',
+			phoneNumber: '+13234247577',
 			email: 'optician1@bonavistaoptics.com',
 			officeLocation: '34.062707,-118.353911',
 			facebookPage: 'https://www.facebook.com/bonavistaoptics'
